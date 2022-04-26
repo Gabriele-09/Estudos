9b2646f13f4d95d400c43ca63fa38f6b5791a10e
Gabriele-09 <gabiribeiro.089@gmail.com>
2022-04-26 17:04:35 -0400

    fix:alteração para o novo projeto

--- resources/views/buscaCep.php
+++ resources/views/buscaCep.php
@@ -2,56 +2,76 @@
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Testando Consumo Api</title>
-    <link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Via Cep API</title>
+    <link href="resources/css/style.css" rel="stylesheet">
 </head>
 
 <body>
-    <div class="campo">
-        <h2 id="titulo">Consultas CEPs</h2>
-        <p id="subtitulo">Digite o CEP para encontrar o endereço</p>
-        <br>
-    </div>
-
-    <form action="{{ url('/cep') }}" method="get">
-        @csrf
-        <fieldset class="grupo">
-            <div class="campo">
-                <label>CEP</label>
-                <input type="text" placeholder="Digite um cep..." name="cep" required>
-                <input type="submit">
+    <form class="form" action="." method="post">
+        <div class="border">
+            <div class="info">
+                <h2 class="titulo">Consulta CEP</h2>
+                <p>Preencha os dados para consulta</p>
             </div>
-        </fieldset>
 
-        <fieldset class="grupo">
-            @if(!is_null($endereco))
-            <h3>Resultados</h3>
-            <div class="campo">
-                <label for="rua">Rua:</label>
+            <div class="data">
+                <label>Nome</label>
+                <input type="text" placeholder="Digite seu nome..." name="nome" required>
+            </div>
+
+            <div class="data">
+                <label>Telefone</label>
+                <input type="text" placeholder="Digite seu telefone..." name="telefone">
+            </div>
+
+            <div class="data">
+                <label>CPF/CNPJ</label>
+                <input type="text" placeholder="Digite seu CPF ou CNPJ..." name="cpfCnpj" required>
+            </div>
+
+            <div class="data">
+                <label>CEP</label>
+                <input type="text" placeholder="Digite seu CEP..." name="cep" required>
+            </div>
+
+            <div class="data btn">
+                <button type="submit">CONSULTAR</button>
+            </div>
+
+            <div class="data">
+                <label>Rua</label>
                 <input type="text" name="rua" value="{{$endereco->logradouro}}">
             </div>
 
-            <div class="campo">
-                <label for="bairro">Bairro:</label>
+            <div class="data">
+                <label>Bairro</label>
                 <input type="text" name="bairro" value="{{$endereco->bairro}}">
             </div>
-            <div>
-                <label for="cidade">Cidade:</label>
+
+            <div class="data">
+                <label>Cidade</label>
                 <input type="text" name="cidade" value="{{$endereco->localidade}}">
             </div>
-            <div class="campo">
-                <label for="estado">Estado:</label>
+
+            <div class="data">
+                <label>Estado</label>
                 <input type="text" name="estado" value="{{$endereco->uf}}">
             </div>
-            @endif
-            @if(is_null($endereco))
-            <h3>Nenhum endereço encontrado!</h3>
-            @endif
-        </fieldset>
+
+            <div class="data">
+                <label>IBGE</label>
+                <input type="text" name="ibge" value="{{$endereco->ibge}}">
+            </div>
+
+        </div>
+
+
     </form>
 
-
 </body>
+
 </html>
+
+https://www.youtube.com/watch?v=lXr4Z7uCufw
